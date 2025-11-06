68db3b52f75dd87e8f42fecfae02c44bce767847
JamesHD001 <henry.jameshd@gmail.com>
2025-11-06 17:35:01 +0100

    update

--- week02/password_strength/password.py
+++ week02/password_strength/password.py
@@ -11,18 +11,23 @@ UPPER = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R"
 DIGITS = ["0","1","2","3","4","5","6","7","8","9"]
 SPECIAL = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "\"", "'", ",", ".", "<", ">", "?", "/", "`", "~"]
 
+#File presence function
 def word_in_file(word, filename, case_sensitive=False):
     pass
 
+#Character presence function
 def word_has_character(word, character_list):
     pass
 
+#Character complexity function
 def word_complexity(word):
     pass
 
+#Password strength function
 def password_strength(password, min_length=10, strong_length=16):
     pass
 
+# Main function to interact with the user
 def main():
     password = input("Enter a password to test it's strength (or type 'q' to quit the program): ")
     while password.lower() != 'q':
